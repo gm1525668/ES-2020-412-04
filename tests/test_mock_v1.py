6ae918a0ffeef6feb6781d1024d827b0c361bb57
Frank <frank.calderon@e-campus.uab.cat>
2020-05-15 18:06:51 +0200

    Creado el test para la gestión del pago del viaje en el archivo test_mock_v1.py

--- tests/test_mock_v1.py
+++ tests/test_mock_v1.py
@@ -1,15 +1,17 @@
 import unittest
-from src.Trip import Trip
 from unittest import mock
+from src.Trip import Trip
+from src.User import User
 
 
 class TestPayment(unittest.TestCase):
     @mock.patch('src.User')
     def test_payment(self, mock_user):
         # set up mock
-        self.assertTrue(mock_user.pay(Trip(2,'barna','callesalamanca', 0, 0, 100)))
+        trip = Trip(2, 'BCN', 'LDN', 0, 0, 100)
+        self.assertTrue(mock_user.pay(trip))
         mock_user.pay.return_value = False
-        self.assertFalse(mock_user.pay(Trip(2,'barna','callesalamanca', 0, 0, 100)))
+        self.assertFalse(mock_user.pay(trip))
 
 
 if __name__ == '__main__':
