20729f5250d26a7419b3041f7ab69765a6069995
gm1525668 <guillem.martinezs@e-campus.uab.es>
2020-05-15 16:14:43 +0200

    User.py: atributos

--- src/User.py
+++ src/User.py
@@ -1,4 +1,6 @@
 class User:
 
-    def __init__(self):
-        pass
+    def __init__(self, id_user, user_email, user_tel):
+        self.id_user = id_user
+        self.user_email = user_email
+        self.user_tel = user_tel
