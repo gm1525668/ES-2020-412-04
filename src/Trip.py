a9a2c789309fcbc5a32d91ff5503996b88790b4f
Frank <frank.calderon@e-campus.uab.cat>
2020-05-16 12:52:47 +0200

    Modificación de la clase Trip.py

--- src/Trip.py
+++ src/Trip.py
@@ -1,3 +1,8 @@
+from src.Flights import Flights
+from src.Hotels import Hotels
+from src.Cars import Cars
+
+
 class Trip:
 
     # num_passengers = Número de pasajeros
@@ -6,7 +11,7 @@ class Trip:
     # start_date = Fecha de salida
     # finish_date = Fecha de llegada
     # price = Precio
-    def __init__(self, num_passengers, origin, flight_list, hotel_list, car_list, start_date, finish_date, price, user):
+    def __init__(self, num_passengers, origin, flight_list, hotel_list, car_list, start_date, finish_date, price):
         self.num_passengers = num_passengers
         self.origin = origin
         self.flight_list = flight_list
@@ -15,16 +20,27 @@ class Trip:
         self.start_date = start_date
         self.finish_date = finish_date
         self.price = price
-        self.user = user
 
-    def add_destination(self, d):
-        if d not in self.destination:
-            self.destination.append(d)
+    def add_destination(self, flight: Flights, hotel: Hotels, car: Cars):
+        if flight not in self.flight_list:
+            self.flight_list.append(flight)
+
+            if hotel is not None & hotel not in self.hotel_list:
+                self.flight_list.append(flight)
+
+            if car is not None & car not in self.car_list:
+                self.car_list.append(car)
         else:
             print('Destino ya seleccionado')
 
-    def remove_destination(self, d):
-        if d not in self.destination:
-            print('Destino no valido para eliminar')
+    def remove_destination(self, flight: Flights, hotel: Hotels, car: Cars):
+        if flight in self.flight_list:
+            self.flight_list.remove(flight)
+
+            if hotel is not None & hotel in self.hotel_list:
+                self.flight_list.remove(flight)
+
+            if car is not None & car in self.car_list:
+                self.car_list.remove(car)
         else:
-            self.destination.remove(d)
+            print('Destino no valido para eliminar')
