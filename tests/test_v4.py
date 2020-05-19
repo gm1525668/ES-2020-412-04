dd975e1bca72139cba08e9db215fbf8535d19b8f
gm1525668 <guillem.martinezs@e-campus.uab.es>
2020-05-19 11:45:02 +0200

    tests: preparación headers test v4

---
+++ tests/test_v4.py
@@ -0,0 +1,10 @@
+# mock test, falta que este dentro de una clase, ojo
+def test_retry_payment():
+    assert True == True  # se paga
+    assert False == False  # no se paga
+
+
+# mock test, falta que este dentro de una clase, ojo
+def test_retry_reserve_car():
+    assert True == True  # se reserva
+    assert False == False  # no se reserva
