d847ce1db788b4afd31a3d6a3780a17c70df0d48
gm1525668 <guillem.martinezs@e-campus.uab.es>
2020-05-19 11:43:30 +0200

    tests: preparación headers test v3

---
+++ tests/test_v3.py
@@ -0,0 +1,41 @@
+def test_car_list():
+    assert True == True  # quitar de lista
+    assert True == True  # poner en la lista
+
+
+def test_recalculate_price_trip_if_add_remove_hotel():
+    assert True == True  # quitar de lista
+    assert True == True  # poner en la lista
+
+def test_hotel_list():
+    assert True == True  # quitar de lista
+    assert True == True  # poner en la lista
+
+
+def test_recalculate_price_trip_if_add_remove_hotel():
+    assert True == True  # quitar de lista
+    assert True == True  # poner en la lista
+
+# mock test, falta que este dentro de una clase, ojo
+def test_reserve_car():
+    assert True == True  # se reserva
+    assert False == False  # no se reserva
+
+# mock test, falta que este dentro de una clase, ojo
+def test_reserve_hotel():
+    assert True == True  # se reserva
+    assert False == False  # no se reserva
+
+# mock test, falta que este dentro de una clase, ojo
+def test_reserve_car():
+    assert True == True  # se reserva
+    assert False == False  # no se reserva
+
+# con matriz de diferentes tests, iva, etc, echad un ojo a consideraciones del pdf
+def test_calculate_price():
+    assert True == True
+    assert True == True
+    assert True == True
+    assert True == True
+    assert True == True
+
