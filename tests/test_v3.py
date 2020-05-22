afea710aec5cffdc553b35815b84c7bb76f60dcd
1527988 <raul.fernandeza@e-campus.uab.cat>
2020-05-22 19:54:01 +0200

    test add y remove hotel

--- tests/test_v3.py
+++ tests/test_v3.py
@@ -82,13 +82,78 @@ def test_recalculate_price_trip_if_add_remove_car():
 
 
 def test_hotel_list():
-    assert True == True  # quitar de lista
-    assert True == True  # poner en la lista
+    num_passengers = 2
+
+    flight1 = Flights(1, 'BRE', num_passengers, 10)
+    flight2 = Flights(2, 'BRU', num_passengers, 20)
+    flight3 = Flights(3, 'MRS', num_passengers, 30)
+    flight4 = Flights(4, 'BCN', num_passengers, 50)
+
+    hotel1 = Hotels(1, 'Hotel 1', num_passengers, 1, 2, 50)
+    hotel2 = Hotels(2, 'Hotel 2', num_passengers, 1, 2, 50)
+    hotel3 = Hotels(3, 'Hotel 3', num_passengers, 1, 2, 50)
+    hotel4 = Hotels(4, 'Hotel 4', num_passengers, 1, 2, 50)
+
+    car1 = Cars(1, 'Honda', 'Airport', 2, 25)
+    car2 = Cars(2, 'Audi', 'Airport', 2, 25)
+    car3 = Cars(3, 'Mercedes', 'Airport', 2, 25)
+    car4 = Cars(4, 'Mazda', 'Airport', 2, 25)
+
+    destination_list = [
+        {'flight': flight1, 'hotel': hotel1, 'car': car1},
+        {'flight': flight2, 'hotel': hotel2, 'car': car2},
+        {'flight': flight3, 'hotel': hotel3, 'car': car3},
+        {'flight': flight4, 'hotel': None, 'car': None}
+    ]
+
+    trip = Trip(num_passengers, 'BCN', destination_list, '01/05/2020', '10/05/2020')
+    trip.add_hotel(trip.get_destination_index(flight4), hotel4)
+
+    result_hotel_list = [hotel1, hotel2, hotel3, hotel4]
+    assert trip.get_hotels() == result_hotel_list
+
+    trip.remove_hotel(trip.get_destination_index(flight4), hotel4)
+    result_hotel_list = [hotel1, hotel2, hotel3, None]
+    assert trip.get_hotels() == result_hotel_list
 
 
 def test_recalculate_price_trip_if_add_remove_hotel():
-    assert True == True  # quitar de lista
-    assert True == True  # poner en la lista
+    num_passengers = 2
+
+    flight1 = Flights(1, 'BRE', num_passengers, 10)
+    flight2 = Flights(2, 'BRU', num_passengers, 20)
+    flight3 = Flights(3, 'MRS', num_passengers, 30)
+    flight4 = Flights(5, 'BCN', num_passengers, 50)
+
+    hotel1 = Hotels(1, 'Hotel 1', num_passengers, 1, 2, 50)
+    hotel2 = Hotels(2, 'Hotel 2', num_passengers, 1, 2, 50)
+    hotel3 = Hotels(3, 'Hotel 3', num_passengers, 1, 2, 50)
+    hotel4 = Hotels(4, 'Hotel 4', num_passengers, 1, 2, 50)
+
+    car1 = Cars(1, 'Honda', 'Airport', 2, 25)
+    car2 = Cars(2, 'Audi', 'Airport', 2, 25)
+    car3 = Cars(3, 'Mercedes', 'Airport', 2, 25)
+    car4 = Cars(4, 'Mazda', 'Airport', 2, 25)
+
+    destination_list = [
+        {'flight': flight1, 'hotel': hotel1, 'car': car1},
+        {'flight': flight2, 'hotel': hotel2, 'car': car2},
+        {'flight': flight3, 'hotel': hotel3, 'car': car3},
+        {'flight': flight4, 'hotel': None, 'car': None}
+    ]
+
+    trip = Trip(num_passengers, 'BCN', destination_list, '01/05/2020', '10/05/2020')
+    trip.add_hotel(trip.get_destination_index(flight4), hotel4)
+    trip.calc_price()
+
+    result_price = 360
+    assert trip.price == result_price
+
+    trip.remove_hotel(trip.get_destination_index(flight4), hotel4)
+    trip.calc_price()
+
+    result_price=335
+    assert trip.price ==result_price
 
 # mock test, falta que este dentro de una clase, ojo
 class testReserveCar(unittest.TestCase):
@@ -130,6 +195,83 @@ class testReserveCar(unittest.TestCase):
 
 
 class testReserveHotel(unittest.TestCase):
+    @mock.patch('src.User.Booking.py')
+    def test_reserve_car(self, mock_Booking):  # se reserva
+        num_passengers = 2
+        flight1 = Flights(1, 'BRE', num_passengers, 10)
+        flight2 = Flights(2, 'BRU', num_passengers, 20)
+        flight3 = Flights(3, 'MRS', num_passengers, 30)
+        flight4 = Flights(4, 'DUB', num_passengers, 40)
+        flight5 = Flights(5, 'LDN', num_passengers, 50)
+        flight6 = Flights(6, 'BCN', num_passengers, 60)
+
+        hotel1 = Hotels(1, 'Hotel 1', num_passengers, 1, 2, 50)
+        hotel2 = Hotels(2, 'Hotel 2', num_passengers, 1, 2, 50)
+        hotel3 = Hotels(3, 'Hotel 3', num_passengers, 1, 2, 50)
+        hotel4 = Hotels(4, 'Hotel 4', num_passengers, 1, 2, 50)
+
+        car1 = Cars(1, 'Honda', 'Airport', 2, 25)
+        car2 = Cars(2, 'Audi', 'Airport', 2, 25)
+        car3 = Cars(3, 'Mercedes', 'Airport', 2, 25)
+        car4 = Cars(4, 'Mazda', 'Airport', 2, 25)
+        destination_list = [
+            {'flight': flight1, 'hotel': hotel1, 'car': car1},
+            {'flight': flight2, 'hotel': hotel2, 'car': car2},
+            {'flight': flight3, 'hotel': hotel3, 'car': car3},
+            {'flight': flight4, 'hotel': hotel4, 'car': car4},
+            {'flight': flight5, 'hotel': None, 'car': None}
+        ]
+
+        trip = Trip(num_passengers, 'BCN', destination_list, '01/05/2020', '10/05/2020')
+        payment_data = PaymentData('VISA', 'Test', '4940190000370787', 1111, 0)
+        user = User(1, 'test@gmail.com', 111111111, payment_data)
+        mock_Booking.return_value.confirm_reserve.return_value = False
+        self.assertFalse(trip.reserve_hotel(user))
+
+
+    assert True == True  # se reserva
+    assert False == False  # no se reserva
+
+# con matriz de diferentes tests, iva, etc, echad un ojo a consideraciones del pdf
+def test_calculate_price():
+    assert True == True
+    assert True == True
+    assert True == True
+    assert True == True
+    assert True == True
+
+
+def test_car_list():
+    assert True == True  # quitar de lista
+    assert True == True  # poner en la lista
+
+
+def test_recalculate_price_trip_if_add_remove_car():
+    assert True == True  # quitar de lista
+    assert True == True  # poner en la lista
+
+def test_hotel_list():
+    assert True == True  # quitar de lista
+    assert True == True  # poner en la lista
+
+
+def test_recalculate_price_trip_if_add_remove_hotel():
+    assert True == True  # quitar de lista
+    assert True == True  # poner en la lista
+
+# mock test, falta que este dentro de una clase, ojo
+def test_reserve_car():
+    assert True == True  # se reserva
+    assert False == False  # no se reserva
+
+# mock test, falta que este dentro de una clase, ojo
+def test_reserve_hotel():
+    assert True == True  # se reserva
+    assert False == False  # no se reserva
+
+# mock test, falta que este dentro de una clase, ojo
+
+class testReserveHotel(unittest.TestCase):
     @mock.patch('src.Trip.Hotels.Booking')
     def test_reserve_Hotel(self, mock_Booking):  # se reserva
         num_passengers = 2
@@ -162,7 +304,6 @@ class testReserveHotel(unittest.TestCase):
         user = User(1, 'test@gmail.com', 111111111, payment_data)
         mock_Booking.return_value.confirm_reserve.return_value = False
         self.assertFalse(trip.reserve_hotel(user))
-
 
     assert True == True  # se reserva
     assert False == False  # no se reserva
