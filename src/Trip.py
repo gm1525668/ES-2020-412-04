069b4292758ebb875baf83ad147d17d5a4bcdba2
Frank <frank.calderon@e-campus.uab.cat>
2020-05-22 18:06:27 +0200

    Modificación de la clase Trip.py y correción del archivo de test test_v1.py

--- src/Trip.py
+++ src/Trip.py
@@ -1,4 +1,3 @@
-
 from . import User
 from . import Flights
 from . import Hotels
@@ -13,123 +12,133 @@ class Trip:
     # start_date = Fecha de salida
     # finish_date = Fecha de llegada
     # price = Precio
-    def __init__(self, num_passengers, origin, flight_list, hotel_list, car_list, start_date, finish_date, price):
+    def __init__(self, num_passengers, origin, destination_list, start_date, finish_date):
         self.num_passengers = num_passengers
         self.origin = origin
-        self.flight_list = flight_list
-        self.hotel_list = hotel_list
-        self.car_list = car_list
+        self.destination_list = destination_list
         self.start_date = start_date
         self.finish_date = finish_date
-        self.price = price
+        self.price = 0
 
     def add_destination(self, flight: Flights, hotel: Hotels, car: Cars):
-         if flight not in self.flight_list:
-            self.flight_list.insert((len(self.flight_list) - 1), flight)
-
-            if hotel is not None and hotel not in self.hotel_list:
-                self.hotel_list.append(hotel)
-
-            if car is not None and car not in self.car_list:
-                self.car_list.append(car)
+        if flight not in self.get_flights():
+            self.destination_list.insert((len(self.destination_list) - 1),
+                                         ({'flight': flight, 'hotel': hotel, 'car': car}))
         else:
-            print('Destino ya seleccionado')
+            print('Error: El destino ya ha sido seleccionado anteriormente.')
 
     def remove_destination(self, flight: Flights, hotel: Hotels, car: Cars):
-        if flight in self.flight_list:
-            self.flight_list.remove(flight)
-
-            if hotel is not None and hotel in self.hotel_list:
-                self.hotel_list.remove(hotel)
-
-            if car is not None and car in self.car_list:
-                self.car_list.remove(car)
+        if flight in self.get_flights():
+            destination_to_remove = next((destination for destination in self.destination_list if destination['flight'] == flight), None)
+            self.destination_list.remove(destination_to_remove)
         else:
-            print('Destino no valido para eliminar')
+            print('Error: El destino no existe en la lista de destinaciones.')
+
+    def get_flights(self):
+        return [destination['flight'] for destination in self.destination_list]
+
+    def get_hotels(self):
+        return [destination['hotel'] for destination in self.destination_list]
+
+    def get_cars(self):
+        return [destination['car'] for destination in self.destination_list]
 
     def calc_price(self):
-        for flight in self.flight_list:
-            self.price += flight.price
+        for flight in self.get_flights():
+            if flight is not None:
+                self.price += flight.price
 
-        for hotel in self.hotel_list:
-            self.price += hotel.price
+        for hotel in self.get_hotels():
+            if hotel is not None:
+                self.price += hotel.price
 
-        for car in self.car_list:
-            self.price += car.price
+        for car in self.get_cars():
+            if car is not None:
+                self.price += car.price
 
     def Confirmar_Vol(self, flight: Flights):
         Confirm = 0
-        for counter, flight in enumerate(self.flight_list):
+        for counter, flight in enumerate(flight):
             if flight.num_passengers != 0 and flight.destination != 'None' and flight.id_flight != 0 and flight.price != 0:
                 print('Confirmación Correcta')
                 Confirm += 1
             elif flight.num_passengers == 0:
-                print('No se ha podido confirmar la reserva de vuelo numero:' + str(counter) + 'debido a que no esta indicado en numero de pasajeros.')
+                print('No se ha podido confirmar la reserva de vuelo numero:' + str(
+                    counter) + 'debido a que no esta indicado en numero de pasajeros.')
             elif flight.destination == 0:
-                print('No se ha podido confirmar la reserva de vuelo numero:' + str(counter) + 'debido a que no esta indicada destinacion.')
+                print('No se ha podido confirmar la reserva de vuelo numero:' + str(
+                    counter) + 'debido a que no esta indicada destinacion.')
             elif flight.id_flight == 0:
-                print('No se ha podido confirmar la reserva de vuelo numero:' + str(counter) + 'debido a que no esta indicada la id del vuelo.')
+                print('No se ha podido confirmar la reserva de vuelo numero:' + str(
+                    counter) + 'debido a que no esta indicada la id del vuelo.')
             elif flight.price == 0:
-                print('No se ha podido confirmar la reserva de vuelo numero:' + str(counter) + 'debido a que no esta indicado el precio.')
-        if Confirm == len(self.flight_list):
+                print('No se ha podido confirmar la reserva de vuelo numero:' + str(
+                    counter) + 'debido a que no esta indicado el precio.')
+        if Confirm == len(self.get_flights()):
             return True
         else:
             return False
+
     def confirmar_coche(self, car: Cars):
         confirm = 0
-        for counter, car in enumerate(self.car_list):
+        for counter, car in enumerate(self.get_cars()):
             if car.id_car != 0 and car.brand != 'None' and car.place != 0 and car.days != 0 and car.price != 0:
                 print('Confirmación Correcta')
                 confirm += 1
             elif car.id_car == 0:
-                print('No se ha podido confirmar la reserva de coche numero:' + str(counter) + 'debido a que no esta indicada la id del coche.')
+                print('No se ha podido confirmar la reserva de coche numero:' + str(
+                    counter) + 'debido a que no esta indicada la id del coche.')
             elif car.brand == 0:
-                print('No se ha podido confirmar la reserva de coche numero:' + str(counter) + 'debido a que no esta indicada la marca del coche.')
+                print('No se ha podido confirmar la reserva de coche numero:' + str(
+                    counter) + 'debido a que no esta indicada la marca del coche.')
             elif car.place == 0:
-                print('No se ha podido confirmar la reserva de coche numero:' + str(counter) + 'debido a que no estan indicadas las plazas del coche.')
+                print('No se ha podido confirmar la reserva de coche numero:' + str(
+                    counter) + 'debido a que no estan indicadas las plazas del coche.')
             elif car.days == 0:
-                print('No se ha podido confirmar la reserva de coche numero:' + str(counter) + 'debido a que no esta indicados los dias de alquiler del coche.')
+                print('No se ha podido confirmar la reserva de coche numero:' + str(
+                    counter) + 'debido a que no esta indicados los dias de alquiler del coche.')
             elif car.price == 0:
-                print('No se ha podido confirmar la reserva de coche numero:' + str(counter) + 'debido a que no esta indicado el precio del coche.')
-        if confirm == len(self.car_list):
+                print('No se ha podido confirmar la reserva de coche numero:' + str(
+                    counter) + 'debido a que no esta indicado el precio del coche.')
+        if confirm == len(self.get_cars()):
             return True
         else:
             return False
 
-    def confirmar_hotel(self,hotel : Hotels):
+    def confirmar_hotel(self, hotel: Hotels):
         confirm = 0
-        for counter, hotel in enumerate(self.hotel_list):
-            if hotel.id_hotel != 'None' and hotel.name !='None' and  hotel.num_guests != 0 and hotel.num_rooms != 0 and hotel.days != 0 and hotel.price != 0:
+        for counter, hotel in enumerate(self.get_hotels()):
+            if hotel.id_hotel != 'None' and hotel.name != 'None' and hotel.num_guests != 0 and hotel.num_rooms != 0 and hotel.days != 0 and hotel.price != 0:
                 print('Confirmación Correcta')
                 confirm += 1
             elif hotel.id_hotel == 0:
-                print('No se ha podido confirmar la reserva de hotel numero:' + str(counter) + 'debido a que no esta indicada la id del hotel.')
+                print('No se ha podido confirmar la reserva de hotel numero:' + str(
+                    counter) + 'debido a que no esta indicada la id del hotel.')
             elif hotel.name == 0:
-                print('No se ha podido confirmar la reserva de hotel numero:' + str( counter) + 'debido a que no esta indicado el nombre del hotel.')
+                print('No se ha podido confirmar la reserva de hotel numero:' + str(
+                    counter) + 'debido a que no esta indicado el nombre del hotel.')
             elif hotel.num_guests == 0:
-                print('No se ha podido confirmar la reserva de hotel numero:' + str(counter) + 'debido a que no esta indicado el numero de personas que residiran en el hotel.')
+                print('No se ha podido confirmar la reserva de hotel numero:' + str(
+                    counter) + 'debido a que no esta indicado el numero de personas que residiran en el hotel.')
             elif hotel.num_rooms == 0:
-                print('No se ha podido confirmar la reserva de hotel numero:' + str(counter) + 'debido a que no esta indicado el numero de habitaciones reservadas.')
+                print('No se ha podido confirmar la reserva de hotel numero:' + str(
+                    counter) + 'debido a que no esta indicado el numero de habitaciones reservadas.')
             elif hotel.days == 0:
-                print('No se ha podido confirmar la reserva de hotel numero:' + str(counter) + 'debido a que no estan indicados los dias de la reserva.')
+                print('No se ha podido confirmar la reserva de hotel numero:' + str(
+                    counter) + 'debido a que no estan indicados los dias de la reserva.')
             elif hotel.price == 0:
-                print('No se ha podido confirmar la reserva de hotel numero:' + str(counter) + 'debido a que no esta indicado el precio del hotel.')
-        if confirm == len(self.hotel_list):
+                print('No se ha podido confirmar la reserva de hotel numero:' + str(
+                    counter) + 'debido a que no esta indicado el precio del hotel.')
+        if confirm == len(self.get_hotels()):
             return True
         else:
             return False
 
-
-
-
-
     def reserve_flights(self, user: User):
-        for flight in self.flight_list:
+        for flight in self.get_flights():
             if not flight.reserve_flight(user):
                 print('Error: No se ha podido reservar el vuelo ' + str(flight.id_flight) + ' correctamente.')
                 return False
 
+        print('Ok: Vuelos reservados correctamente.')
         return True
-
-
-
--- tests/test_v1.py
+++ tests/test_v1.py
@@ -11,14 +11,14 @@ from src.Cars import Cars
 
 @pytest.mark.parametrize('num_passengers, result', [(2, 2), (3, 3), (4, 4), (52, 52)])
 def test_num_passengers(num_passengers, result):
-    trip = Trip(num_passengers, 'BCN', [], [], [], '01/05/2020', '10/05/2020', 0)
+    trip = Trip(num_passengers, 'BCN', [], '01/05/2020', '10/05/2020')
     assert trip.num_passengers == result
 
 
 def test_no_destination_flight_list_empty():
     num_passengers = 2
-    trip = Trip(num_passengers, 'BCN', [], [], [], '01/05/2020', '10/05/2020', 0)
-    assert trip.flight_list == []
+    trip = Trip(num_passengers, 'BCN', [], '01/05/2020', '10/05/2020')
+    assert trip.get_flights() == []
 
 
 def test_no_destination_flights():
@@ -28,7 +28,7 @@ def test_no_destination_flights():
 def test_no_destinations_price_zero():
     num_passengers = 2
 
-    trip = Trip(num_passengers, 'BCN', [], [], [], '01/05/2020', '10/05/2020', 0)
+    trip = Trip(num_passengers, 'BCN', [], '01/05/2020', '10/05/2020')
     trip.calc_price()
 
     assert trip.price == 0
@@ -41,30 +41,30 @@ def test_add_destination_flight_list():
     flight2 = Flights(2, 'BRU', num_passengers, 20)
     flight3 = Flights(3, 'MRS', num_passengers, 30)
     flight4 = Flights(4, 'DUB', num_passengers, 40)
-    flight5 = Flights(5, 'LDN', num_passengers, 50)
-    flight6 = Flights(6, 'BCN', num_passengers, 60)
-
-    flights_list = [flight1, flight2, flight3, flight4, flight6]
+    flight5 = Flights(5, 'BCN', num_passengers, 50)
 
     hotel1 = Hotels(1, 'Hotel 1', num_passengers, 1, 2, 50)
     hotel2 = Hotels(2, 'Hotel 2', num_passengers, 1, 2, 50)
     hotel3 = Hotels(3, 'Hotel 3', num_passengers, 1, 2, 50)
     hotel4 = Hotels(4, 'Hotel 4', num_passengers, 1, 2, 50)
 
-    hotels_list = [hotel1, hotel2, hotel3]
-
     car1 = Cars(1, 'Honda', 'Airport', 2, 25)
     car2 = Cars(2, 'Audi', 'Airport', 2, 25)
     car3 = Cars(3, 'Mercedes', 'Airport', 2, 25)
     car4 = Cars(4, 'Mazda', 'Airport', 2, 25)
 
-    cars_list = [car1, car2, car3]
+    destination_list = [
+        {'flight': flight1, 'hotel': hotel1, 'car': car1},
+        {'flight': flight2, 'hotel': hotel2, 'car': car2},
+        {'flight': flight3, 'hotel': hotel3, 'car': car3},
+        {'flight': flight5, 'hotel': None, 'car': None}
+    ]
 
-    trip = Trip(num_passengers, 'BCN', flights_list, hotels_list, cars_list, '01/05/2020', '10/05/2020', 0)
-    trip.add_destination(flight5, hotel4, car4)
+    trip = Trip(num_passengers, 'BCN', destination_list, '01/05/2020', '10/05/2020')
+    trip.add_destination(flight4, hotel4, car4)
 
-    result_flights_list = [flight1, flight2, flight3, flight4, flight5, flight6]
-    assert trip.flight_list == result_flights_list
+    result_flights_list = [flight1, flight2, flight3, flight4, flight5]
+    assert trip.get_flights() == result_flights_list
 
 
 def test_calc_price():
@@ -74,29 +74,30 @@ def test_calc_price():
     flight2 = Flights(2, 'BRU', num_passengers, 20)
     flight3 = Flights(3, 'MRS', num_passengers, 30)
     flight4 = Flights(4, 'DUB', num_passengers, 40)
-    flight5 = Flights(5, 'LDN', num_passengers, 50)
-    flight6 = Flights(6, 'BCN', num_passengers, 60)
-
-    flights_list = [flight1, flight2, flight3, flight4, flight5, flight6]
+    flight5 = Flights(5, 'BCN', num_passengers, 50)
 
     hotel1 = Hotels(1, 'Hotel 1', num_passengers, 1, 2, 50)
     hotel2 = Hotels(2, 'Hotel 2', num_passengers, 1, 2, 50)
     hotel3 = Hotels(3, 'Hotel 3', num_passengers, 1, 2, 50)
     hotel4 = Hotels(4, 'Hotel 4', num_passengers, 1, 2, 50)
 
-    hotels_list = [hotel1, hotel2, hotel3, hotel4]
-
     car1 = Cars(1, 'Honda', 'Airport', 2, 25)
     car2 = Cars(2, 'Audi', 'Airport', 2, 25)
     car3 = Cars(3, 'Mercedes', 'Airport', 2, 25)
     car4 = Cars(4, 'Mazda', 'Airport', 2, 25)
 
-    cars_list = [car1, car2, car3, car4]
+    destination_list = [
+        {'flight': flight1, 'hotel': hotel1, 'car': car1},
+        {'flight': flight2, 'hotel': hotel2, 'car': car2},
+        {'flight': flight3, 'hotel': hotel3, 'car': car3},
+        {'flight': flight4, 'hotel': hotel4, 'car': car4},
+        {'flight': flight5, 'hotel': None, 'car': None}
+    ]
 
-    trip = Trip(num_passengers, 'BCN', flights_list, hotels_list, cars_list, '01/05/2020', '10/05/2020', 0)
+    trip = Trip(num_passengers, 'BCN', destination_list, '01/05/2020', '10/05/2020')
     trip.calc_price()
 
-    result_price = 510
+    result_price = 450
     assert trip.price == result_price
 
 
@@ -107,30 +108,30 @@ def test_add_destination_calc_price():
     flight2 = Flights(2, 'BRU', num_passengers, 20)
     flight3 = Flights(3, 'MRS', num_passengers, 30)
     flight4 = Flights(4, 'DUB', num_passengers, 40)
-    flight5 = Flights(5, 'LDN', num_passengers, 50)
-    flight6 = Flights(6, 'BCN', num_passengers, 60)
-
-    flights_list = [flight1, flight2, flight3, flight4, flight6]
+    flight5 = Flights(5, 'BCN', num_passengers, 50)
 
     hotel1 = Hotels(1, 'Hotel 1', num_passengers, 1, 2, 50)
     hotel2 = Hotels(2, 'Hotel 2', num_passengers, 1, 2, 50)
     hotel3 = Hotels(3, 'Hotel 3', num_passengers, 1, 2, 50)
     hotel4 = Hotels(4, 'Hotel 4', num_passengers, 1, 2, 50)
 
-    hotels_list = [hotel1, hotel2, hotel3]
-
     car1 = Cars(1, 'Honda', 'Airport', 2, 25)
     car2 = Cars(2, 'Audi', 'Airport', 2, 25)
     car3 = Cars(3, 'Mercedes', 'Airport', 2, 25)
     car4 = Cars(4, 'Mazda', 'Airport', 2, 25)
 
-    cars_list = [car1, car2, car3]
+    destination_list = [
+        {'flight': flight1, 'hotel': hotel1, 'car': car1},
+        {'flight': flight2, 'hotel': hotel2, 'car': car2},
+        {'flight': flight3, 'hotel': hotel3, 'car': car3},
+        {'flight': flight5, 'hotel': None, 'car': None}
+    ]
 
-    trip = Trip(num_passengers, 'BCN', flights_list, hotels_list, cars_list, '01/05/2020', '10/05/2020', 0)
-    trip.add_destination(flight5, hotel4, car4)
+    trip = Trip(num_passengers, 'BCN', destination_list, '01/05/2020', '10/05/2020')
+    trip.add_destination(flight4, hotel4, car4)
     trip.calc_price()
 
-    result_price = 510
+    result_price = 450
     assert trip.price == result_price
 
 
@@ -141,30 +142,31 @@ def test_remove_destination_flight_list():
     flight2 = Flights(2, 'BRU', num_passengers, 20)
     flight3 = Flights(3, 'MRS', num_passengers, 30)
     flight4 = Flights(4, 'DUB', num_passengers, 40)
-    flight5 = Flights(5, 'LDN', num_passengers, 50)
-    flight6 = Flights(6, 'BCN', num_passengers, 60)
-
-    flights_list = [flight1, flight2, flight3, flight4, flight5, flight6]
+    flight5 = Flights(5, 'BCN', num_passengers, 50)
 
     hotel1 = Hotels(1, 'Hotel 1', num_passengers, 1, 2, 50)
     hotel2 = Hotels(2, 'Hotel 2', num_passengers, 1, 2, 50)
     hotel3 = Hotels(3, 'Hotel 3', num_passengers, 1, 2, 50)
     hotel4 = Hotels(4, 'Hotel 4', num_passengers, 1, 2, 50)
 
-    hotels_list = [hotel1, hotel2, hotel3, hotel4]
-
     car1 = Cars(1, 'Honda', 'Airport', 2, 25)
     car2 = Cars(2, 'Audi', 'Airport', 2, 25)
     car3 = Cars(3, 'Mercedes', 'Airport', 2, 25)
     car4 = Cars(4, 'Mazda', 'Airport', 2, 25)
 
-    cars_list = [car1, car2, car3, car4]
+    destination_list = [
+        {'flight': flight1, 'hotel': hotel1, 'car': car1},
+        {'flight': flight2, 'hotel': hotel2, 'car': car2},
+        {'flight': flight3, 'hotel': hotel3, 'car': car3},
+        {'flight': flight4, 'hotel': hotel4, 'car': car4},
+        {'flight': flight5, 'hotel': None, 'car': None}
+    ]
 
-    trip = Trip(num_passengers, 'BCN', flights_list, hotels_list, cars_list, '01/05/2020', '10/05/2020', 0)
-    trip.remove_destination(flight5, hotel4, car4)
+    trip = Trip(num_passengers, 'BCN', destination_list, '01/05/2020', '10/05/2020')
+    trip.remove_destination(flight4, hotel4, car4)
 
-    result_flights_list = [flight1, flight2, flight3, flight4, flight6]
-    assert trip.flight_list == result_flights_list
+    result_flights_list = [flight1, flight2, flight3, flight5]
+    assert trip.get_flights() == result_flights_list
 
 
 def test_remove_destination_calc_price():
@@ -174,30 +176,31 @@ def test_remove_destination_calc_price():
     flight2 = Flights(2, 'BRU', num_passengers, 20)
     flight3 = Flights(3, 'MRS', num_passengers, 30)
     flight4 = Flights(4, 'DUB', num_passengers, 40)
-    flight5 = Flights(5, 'LDN', num_passengers, 50)
-    flight6 = Flights(6, 'BCN', num_passengers, 60)
-
-    flights_list = [flight1, flight2, flight3, flight4, flight5, flight6]
+    flight5 = Flights(5, 'BCN', num_passengers, 50)
 
     hotel1 = Hotels(1, 'Hotel 1', num_passengers, 1, 2, 50)
     hotel2 = Hotels(2, 'Hotel 2', num_passengers, 1, 2, 50)
     hotel3 = Hotels(3, 'Hotel 3', num_passengers, 1, 2, 50)
     hotel4 = Hotels(4, 'Hotel 4', num_passengers, 1, 2, 50)
 
-    hotels_list = [hotel1, hotel2, hotel3, hotel4]
-
     car1 = Cars(1, 'Honda', 'Airport', 2, 25)
     car2 = Cars(2, 'Audi', 'Airport', 2, 25)
     car3 = Cars(3, 'Mercedes', 'Airport', 2, 25)
     car4 = Cars(4, 'Mazda', 'Airport', 2, 25)
 
-    cars_list = [car1, car2, car3, car4]
+    destination_list = [
+        {'flight': flight1, 'hotel': hotel1, 'car': car1},
+        {'flight': flight2, 'hotel': hotel2, 'car': car2},
+        {'flight': flight3, 'hotel': hotel3, 'car': car3},
+        {'flight': flight4, 'hotel': hotel4, 'car': car4},
+        {'flight': flight5, 'hotel': None, 'car': None}
+    ]
 
-    trip = Trip(num_passengers, 'BCN', flights_list, hotels_list, cars_list, '01/05/2020', '10/05/2020', 0)
-    trip.remove_destination(flight5, hotel4, car4)
+    trip = Trip(num_passengers, 'BCN', destination_list, '01/05/2020', '10/05/2020')
+    trip.remove_destination(flight4, hotel4, car4)
     trip.calc_price()
 
-    result_price = 385
+    result_price = 335
     assert trip.price == result_price
 
 
@@ -208,26 +211,27 @@ def test_confirm_pay():
     flight2 = Flights(2, 'BRU', num_passengers, 20)
     flight3 = Flights(3, 'MRS', num_passengers, 30)
     flight4 = Flights(4, 'DUB', num_passengers, 40)
-    flight5 = Flights(5, 'LDN', num_passengers, 50)
-    flight6 = Flights(6, 'BCN', num_passengers, 60)
-
-    flights_list = [flight1, flight2, flight3, flight4, flight5, flight6]
+    flight5 = Flights(5, 'BCN', num_passengers, 50)
 
     hotel1 = Hotels(1, 'Hotel 1', num_passengers, 1, 2, 50)
     hotel2 = Hotels(2, 'Hotel 2', num_passengers, 1, 2, 50)
     hotel3 = Hotels(3, 'Hotel 3', num_passengers, 1, 2, 50)
     hotel4 = Hotels(4, 'Hotel 4', num_passengers, 1, 2, 50)
 
-    hotels_list = [hotel1, hotel2, hotel3, hotel4]
-
     car1 = Cars(1, 'Honda', 'Airport', 2, 25)
     car2 = Cars(2, 'Audi', 'Airport', 2, 25)
     car3 = Cars(3, 'Mercedes', 'Airport', 2, 25)
     car4 = Cars(4, 'Mazda', 'Airport', 2, 25)
 
-    cars_list = [car1, car2, car3, car4]
+    destination_list = [
+        {'flight': flight1, 'hotel': hotel1, 'car': car1},
+        {'flight': flight2, 'hotel': hotel2, 'car': car2},
+        {'flight': flight3, 'hotel': hotel3, 'car': car3},
+        {'flight': flight4, 'hotel': hotel4, 'car': car4},
+        {'flight': flight5, 'hotel': None, 'car': None}
+    ]
 
-    trip = Trip(num_passengers, 'BCN', flights_list, hotels_list, cars_list, '01/05/2020', '10/05/2020', 0)
+    trip = Trip(num_passengers, 'BCN', destination_list, '01/05/2020', '10/05/2020')
 
     payment_data = PaymentData('VISA', 'Test', '4940190000370787', 1111, 0)
     user = User(1, 'test@gmail.com', 111111111, payment_data)
@@ -244,26 +248,27 @@ class testPayment(unittest.TestCase):
         flight2 = Flights(2, 'BRU', num_passengers, 20)
         flight3 = Flights(3, 'MRS', num_passengers, 30)
         flight4 = Flights(4, 'DUB', num_passengers, 40)
-        flight5 = Flights(5, 'LDN', num_passengers, 50)
-        flight6 = Flights(6, 'BCN', num_passengers, 60)
-
-        flights_list = [flight1, flight2, flight3, flight4, flight5, flight6]
+        flight5 = Flights(5, 'BCN', num_passengers, 50)
 
         hotel1 = Hotels(1, 'Hotel 1', num_passengers, 1, 2, 50)
         hotel2 = Hotels(2, 'Hotel 2', num_passengers, 1, 2, 50)
         hotel3 = Hotels(3, 'Hotel 3', num_passengers, 1, 2, 50)
         hotel4 = Hotels(4, 'Hotel 4', num_passengers, 1, 2, 50)
 
-        hotels_list = [hotel1, hotel2, hotel3, hotel4]
-
         car1 = Cars(1, 'Honda', 'Airport', 2, 25)
         car2 = Cars(2, 'Audi', 'Airport', 2, 25)
         car3 = Cars(3, 'Mercedes', 'Airport', 2, 25)
         car4 = Cars(4, 'Mazda', 'Airport', 2, 25)
 
-        cars_list = [car1, car2, car3, car4]
+        destination_list = [
+            {'flight': flight1, 'hotel': hotel1, 'car': car1},
+            {'flight': flight2, 'hotel': hotel2, 'car': car2},
+            {'flight': flight3, 'hotel': hotel3, 'car': car3},
+            {'flight': flight4, 'hotel': hotel4, 'car': car4},
+            {'flight': flight5, 'hotel': None, 'car': None}
+        ]
 
-        trip = Trip(num_passengers, 'BCN', flights_list, hotels_list, cars_list, '01/05/2020', '10/05/2020', 0)
+        trip = Trip(num_passengers, 'BCN', destination_list, '01/05/2020', '10/05/2020')
 
         payment_data = PaymentData('VISA', 'Test', '4940190000370787', 1111, 0)
         user = User(1, 'test@gmail.com', 111111111, payment_data)
@@ -272,17 +277,37 @@ class testPayment(unittest.TestCase):
         self.assertFalse(user.pay(trip))
 
 
-def test_confirm_vol():
-    num_passenger = 2
-    flight1 = Flights(1, 'BRE', num_passenger, 10)
-    flight2 = Flights(2, 'BRU', num_passenger, 20)
-    flight3 = Flights(3, 'MRS', num_passenger, 20)
-    flight4 = Flights(4, 'DUB', num_passenger, 40)
-    flight5 = Flights(5, 'LDN', num_passenger, 50)
-    flight6 = Flights(6, 'BCN', num_passenger, 60)
-    flights_list = [flight1, flight2, flight3, flight4, flight5, flight6]
-    trip = Trip(num_passenger, 'BCN', flights_list, [], [], '01/05/2020', '10/05/2020', 0)
-    Confirm = trip.Confirmar_Vol(flights_list)
+# ARREGLAR ESTA PUTA MIERDA
+def test_confirm_flight():
+    num_passengers = 2
+
+    flight1 = Flights(1, 'BRE', num_passengers, 10)
+    flight2 = Flights(2, 'BRU', num_passengers, 20)
+    flight3 = Flights(3, 'MRS', num_passengers, 30)
+    flight4 = Flights(4, 'DUB', num_passengers, 40)
+    flight5 = Flights(5, 'BCN', num_passengers, 50)
+
+    hotel1 = Hotels(1, 'Hotel 1', num_passengers, 1, 2, 50)
+    hotel2 = Hotels(2, 'Hotel 2', num_passengers, 1, 2, 50)
+    hotel3 = Hotels(3, 'Hotel 3', num_passengers, 1, 2, 50)
+    hotel4 = Hotels(4, 'Hotel 4', num_passengers, 1, 2, 50)
+
+    car1 = Cars(1, 'Honda', 'Airport', 2, 25)
+    car2 = Cars(2, 'Audi', 'Airport', 2, 25)
+    car3 = Cars(3, 'Mercedes', 'Airport', 2, 25)
+    car4 = Cars(4, 'Mazda', 'Airport', 2, 25)
+
+    destination_list = [
+        {'flight': flight1, 'hotel': hotel1, 'car': car1},
+        {'flight': flight2, 'hotel': hotel2, 'car': car2},
+        {'flight': flight3, 'hotel': hotel3, 'car': car3},
+        {'flight': flight4, 'hotel': hotel4, 'car': car4},
+        {'flight': flight5, 'hotel': None, 'car': None}
+    ]
+
+    trip = Trip(num_passengers, 'BCN', destination_list, '01/05/2020', '10/05/2020')
+
+    Confirm = trip.Confirmar_Vol(trip.get_flights())
     Result = True
     assert Confirm == Result
 
@@ -294,26 +319,27 @@ def test_reserve_flight():
     flight2 = Flights(2, 'BRU', num_passengers, 20)
     flight3 = Flights(3, 'MRS', num_passengers, 30)
     flight4 = Flights(4, 'DUB', num_passengers, 40)
-    flight5 = Flights(5, 'LDN', num_passengers, 50)
-    flight6 = Flights(6, 'BCN', num_passengers, 60)
-
-    flights_list = [flight1, flight2, flight3, flight4, flight5, flight6]
+    flight5 = Flights(5, 'BCN', num_passengers, 50)
 
     hotel1 = Hotels(1, 'Hotel 1', num_passengers, 1, 2, 50)
     hotel2 = Hotels(2, 'Hotel 2', num_passengers, 1, 2, 50)
     hotel3 = Hotels(3, 'Hotel 3', num_passengers, 1, 2, 50)
     hotel4 = Hotels(4, 'Hotel 4', num_passengers, 1, 2, 50)
 
-    hotels_list = [hotel1, hotel2, hotel3, hotel4]
-
     car1 = Cars(1, 'Honda', 'Airport', 2, 25)
     car2 = Cars(2, 'Audi', 'Airport', 2, 25)
     car3 = Cars(3, 'Mercedes', 'Airport', 2, 25)
     car4 = Cars(4, 'Mazda', 'Airport', 2, 25)
 
-    cars_list = [car1, car2, car3, car4]
+    destination_list = [
+        {'flight': flight1, 'hotel': hotel1, 'car': car1},
+        {'flight': flight2, 'hotel': hotel2, 'car': car2},
+        {'flight': flight3, 'hotel': hotel3, 'car': car3},
+        {'flight': flight4, 'hotel': hotel4, 'car': car4},
+        {'flight': flight5, 'hotel': None, 'car': None}
+    ]
 
-    trip = Trip(num_passengers, 'BCN', flights_list, hotels_list, cars_list, '01/05/2020', '10/05/2020', 0)
+    trip = Trip(num_passengers, 'BCN', destination_list, '01/05/2020', '10/05/2020')
 
     payment_data = PaymentData('VISA', 'Test', '4940190000370787', 1111, 0)
     user = User(1, 'test@gmail.com', 111111111, payment_data)
